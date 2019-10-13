3d5b53f551ea75459f1cc5cb4bcc3be91f3d5903
Maxchen <1174344944@qq.com>
2019-10-13 22:49:40 +0800

    fix some bug!

--- eurekaclient/src/main/java/com/maxchen/demo/HelloController.java
+++ eurekaclient/src/main/java/com/maxchen/demo/HelloController.java
@@ -1,13 +1,13 @@
 package com.maxchen.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
 
-    @Value("${server.port}")
+//    @Value("${server.port}")
     private int port;
 
     @RequestMapping("index")
