a367e39177aa7646bb5605023da326796ccb617c
Maxchen <1174344944@qq.com>
2019-10-14 22:53:07 +0800

    fix some bug!

--- eurekaclient/src/main/java/com/maxchen/demo/HelloController.java
+++ eurekaclient/src/main/java/com/maxchen/demo/HelloController.java
@@ -1,13 +1,16 @@
 package com.maxchen.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RefreshScope //以这种方式注释的bean可以在运行时刷新（可以在程序运行时刷新配置文件）
 public class HelloController {
 
-//    @Value("${server.port}")
+    @Value("${server.port}")
     private int port;
 
     @RequestMapping("index")
